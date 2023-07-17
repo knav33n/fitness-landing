4d14c7a8c2f71d596ee6352dffffe9b1193608e1
knav33n <13643224+knav33n@users.noreply.github.com>
2023-07-17 13:59:35 +0530

    fix: change region to us-east-1

--- infra/main.tf
+++ infra/main.tf
@@ -13,11 +13,11 @@ terraform {
   backend "s3" {
     bucket = "gym-landing-terraform-state"
     key    = "gym-landing-terraform-state-key"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-  alias  = "ap-south-1"
-  region = "ap-south-1"
+  alias  = "us-east-1"
+  region = "us-east-1"
 }
